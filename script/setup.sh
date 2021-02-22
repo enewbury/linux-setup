438124536512d8d9f5e61949cc08a355bd3181bd
Eric Newbury <eric@newbury.cloud>
2021-02-21 21:29:17 -0500

    enable wayland

--- script/setup.sh
+++ script/setup.sh
@@ -8,4 +8,13 @@ script/install.sh
 dconf load /org/gnome/ <.config/dconf/settings.dconf
 gsettings set org.gnome.desktop.background picture-uri "file://${HOME}/Downloads/desktop-assets/wallpaper.jpg"
 
+# Enable Wayland
+if [[ -e /etc/gdm3/custom.conf ]]; then
+    sudo sed -i -e 's|.*WaylandEnable=false|#WaylandEnable=false|g' /etc/gdm3/custom.conf
+fi
+
+if ! grep -q "MOZ_ENABLE_WAYLAND" "/etc/environment"; then
+    sudo echo "MOZ_ENABLE_WAYLAND=1" >>/etc/environment
+fi
+
 rcup -d ~/src/github.com/enewbury/dotfiles rcrc && rcup
