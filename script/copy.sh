a40b95a697e8e29ade8778187b11d0558ff5ab39
Eric Newbury <eric@newbury.cloud>
2021-02-21 21:21:29 -0500

    copy gitconfig

--- script/copy.sh
+++ script/copy.sh
@@ -7,3 +7,7 @@ cp -r .themes ${HOME}
 if [ ! -f "$HOME/.hex/hex.config" ]; then
     cp $HOME/Downloads/desktop-assets/hex.config $HOME/.hex/hex.config
 fi
+
+if [ ! -f "$HOME/.gitconfig" ]; then
+    cp $HOME/Downloads/desktop-assets/.gitconfig $HOME/.gitconfig
+fi
